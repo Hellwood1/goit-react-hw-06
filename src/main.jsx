b50a66e64eb4c86338ca24a191b685da6d33fdaa
Hellwood1 <samburik123@gmail.com>
2024-06-08 23:33:40 +0200

    Upd. fix

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from './components/App/App'
 import './index.css'
 
 import { Provider } from 'react-redux'
-import { store, persistor } from './redux/storeSlice'
+import { store, persistor } from './redux/store'
 import { PersistGate } from 'redux-persist/integration/react';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
